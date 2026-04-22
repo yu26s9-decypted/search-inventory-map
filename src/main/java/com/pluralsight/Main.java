c15046f759826de937e071414593e93a7c7fc5f5
andy <andyt.cyb@gmail.com>
2026-04-22 12:42:53 -0400

    added file writer

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,15 +5,129 @@ import ui.Console;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 
 
 public class Main {
     public static void main(String[] arg) {
+        HashMap<Integer, Product> product = dataFileManager(); // This method loads all of our product from our data product.txt into the hashmap
+
+
+        if (product == null) {
+            System.out.println("A fatal error has occured.");
+            return;
+        }
+
+
+        String productSystemMenuMsg = """
+                What do you want to do?
+                1 - List all product
+                2 - Lookup product by Id
+                3 - Find all product within a price range
+                4 - Add a new product
+                5 - Quit application
+                """;
+
+        while (true) {
+            System.out.println(productSystemMenuMsg);
+            int userInput = Console.askForInt("Enter your command:", 1, 5);
+            switch (userInput) {
+                case 1:
+                    processListAllProducts(product);
+                    break;
+                case 2:
+                    processLookupProductById(product);
+                    break;
+                case 3:
+                    processFindProductWithinPriceRange(product);
+                    break;
+                case 4:
+                    addNewProduct(product);
+                    break;
+                case 5:
+                    System.out.println("Thank you have a nice day!");
+                    return;
+            }
+
+
+        }
+
+
+    }
+
+    public static void processListAllProducts(HashMap<Integer, Product> product) {
+        for (Product p : product.values()) {
+            System.out.println(p);
+        }
+
+    }
+
+    public static void processLookupProductById(HashMap<Integer, Product> products) {
+        int getProductId = Console.askForInt("What is the product ID?", 1000, 9999);
+
+        for (Product p : products.values()) {
+            if (p.getProductId() == getProductId) {
+                System.out.printf("Found: [ID: %d] [PRODUCT: %s] [RETAIL PRICE: $%.2f]\n",
+                        p.getProductId(),
+                        p.getProductName(),
+                        p.getProductPrice()
+                );
+                return;
+            }
+        }
+        System.out.printf("Cannot find a product with this id. \n");
+
+    }
+
+    public static HashMap<Integer, Product> processFindProductWithinPriceRange(HashMap<Integer, Product> product) {
+        double askMinPrice = Console.askForDouble("What's the minimum price");
+        double askMaxPrice = Console.askForDouble("What's the maximum price");
+
+        HashMap<Integer, Product> matchingProduct = new HashMap<>();
+
+        for (Product p : product.values()) {
+            if (askMaxPrice >= (p.getProductPrice()) && askMinPrice <= (p.getProductPrice())) {
+                matchingProduct.put(p.getProductId(), p);
+                System.out.printf("Found: [ID: %d] [PRODUCT: %s] [RETAIL PRICE: $%.2f]\n",
+                        p.getProductId(),
+                        p.getProductName(),
+                        p.getProductPrice()
+                );
+
+            }
+
+        }
+        if (matchingProduct.isEmpty()) {
+            System.out.printf("No item found within your price range");
+        }
+        System.out.println("Found " + matchingProduct.size() + (matchingProduct.size() == 1 ? " result." : " results."));
+        return matchingProduct;
+    }
+
+
+    public static void addNewProduct(HashMap<Integer, Product> product) {
+        int addNewProductId = Console.askForInt("What's the product id: ", 1, 9999);
+        while(product.containsKey(addNewProductId)) {
+            System.out.println("This product id already exist! Try again");
+            addNewProductId = Console.askForInt("What's the product id: ", 1, 9999);
+        }
+
+        String addNewProductName = Console.askForString("What's the product name: ");
+        double addNewProductPrice = Console.askForDouble("What's the price of this product: ");
+
+        Product addNewProduct = new Product(addNewProductId, addNewProductName, addNewProductPrice);
+        product.put(addNewProductId, addNewProduct);
+        dataFileWriter(product);
+        System.out.printf("Product inventory has been updated with: %d %s $%.2f \n", addNewProductId, addNewProductName, addNewProductPrice);
+
+
+
+    }
+
+    public static HashMap<Integer, Product> dataFileManager() {
         HashMap<Integer, Product> product = new HashMap<>();
-
-
         try {
             String fileName = "src/main/java/com/pluralsight/data/product.txt";
             FileReader fileReader = new FileReader(fileName);
@@ -34,100 +148,32 @@ public class Main {
                 }
             }
 
-        } catch (IOException e ){
-            System.out.println("Something went wrong with trying read the file.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong with trying read the file. Please check to make sure the location is correct.");
+            return null;
         }
-
-
-
-
-        String productSystemMenuMsg = """
-            What do you want to do?
-            1 - List all product
-            2 - Lookup product by Id
-            3 - Find all product within a price range
-            4 - Add a new product
-            5 - Quit application
-            """;
-
-        while (true){
-            System.out.println(productSystemMenuMsg);
-            int userInput = Console.askForInt("Enter your command:", 1, 5);
-            switch (userInput){
-                case 1:
-                    processListAllProducts(product);
-                    break;
-                case 2:
-                    processLookupProductById(product);
-                    break;
-                case 3:
-                    processFindProductWithinPriceRange(product);
-                    break;
-                case 5:
-                    System.out.println("Thank you have a nice day!");
-                    return;
-            }
-
-
-        }
-
-
-
-    }
-    public static void processListAllProducts(HashMap<Integer, Product> products){
-        for( Product p : products.values()) {
-            System.out.println(p);
-        }
-
+        return product;
     }
 
-    public static void processLookupProductById(HashMap<Integer, Product> products) {
-        int getProductId = Console.askForInt("What is the product ID?", 2000, 5000);
 
-        for (Product p : products.values())
-        {
-            if (p.getProductId() == getProductId ){
-                System.out.printf("Found: [ID: %d] [PRODUCT: %s] [RETAIL PRICE: $%.2f]\n",
-                        p.getProductId(),
-                        p.getProductName(),
-                        p.getProductPrice()
-                );
-                return;
-            }
-        }
-        System.out.printf("Cannot find a product with this id. \n");
+    public static void dataFileWriter(HashMap<Integer, Product> product) {
+        try {
+            String fileName = "src/main/java/com/pluralsight/data/product.txt";
+            FileWriter fileWriter = new FileWriter(fileName, true);
 
-    }
+            for (Product p : product.values()) {
+                String formattedLine = p.getProductId() + "|" +
+                        p.getProductName() + "|" +
+                        p.getProductPrice() + "\n";
 
-    public static HashMap<Integer, Product> processFindProductWithinPriceRange(HashMap<Integer, Product> products) {
-        double askMinPrice = Console.askForDouble("What's the minimum price");
-        double askMaxPrice = Console.askForDouble("What's the maximum price");
-
-        HashMap<Integer, Product> matchingProduct = new HashMap<>();
-
-        for(Product p: products.values())
-        {
-            if (askMaxPrice >= (p.getProductPrice()) && askMinPrice <= (p.getProductPrice())){
-                matchingProduct.put(p.getProductId(), p);
-                System.out.printf("Found: [ID: %d] [PRODUCT: %s] [RETAIL PRICE: $%.2f]\n",
-                        p.getProductId(),
-                        p.getProductName(),
-                        p.getProductPrice()
-                );
+                fileWriter.write(formattedLine);
 
             }
+            fileWriter.close();
 
+        } catch (IOException err) {
+            System.out.println("An error occured" + err.getMessage());
         }
-        if(matchingProduct.isEmpty()) {
-            System.out.printf("No item found within your price range");
-        }
-        System.out.println("Found " + matchingProduct.size() + (matchingProduct.size() == 1 ? " result." : " results." ));
-        return matchingProduct;
     }
-
-
-
-
 }
-
 
